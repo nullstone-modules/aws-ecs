0bf7b831693ef1ba286536f9e98ed5673e42f34d
Brad Sickles <brad.sickles@gmail.com>
2023-07-25 17:26:44 -0400

    Emit capacity provider (#2)

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "deployers_name" {
   value       = aws_iam_group.deployers.name
   description = "string ||| Name of the deployers IAM Group that is allowed to deploy to the ECS cluster."
 }
+
+output "capacity_provider_name" {
+  value       = aws_ecs_capacity_provider.this.name
+  description = "string ||| The name of the capacity provider used by the cluster. This provides a hook into the same compute resources for provisioning ECS tasks."
+}
